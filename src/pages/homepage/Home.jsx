84762591e802196d9f745f11581c31255b75f44a
sulakshana borsune <sulakshana.borsune@gmail.com>
2022-03-12 12:44:14 -0800

    added footer

--- src/pages/homepage/Home.jsx
+++ src/pages/homepage/Home.jsx
@@ -1,10 +1,14 @@
 import Navbar from "../../partial/navbar/navbar";
+import Footer from "../../partial/footer/footer";
+import './homepage.css'
 
 function Home() {
 	return (
 		<>
+		<h1>Messy Museum</h1>
 			<Navbar />
 			Home Page
+			<Footer/>
 		</>
 	);
 }
